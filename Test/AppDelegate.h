0f7288fb3116c5db7a39559609c717a7780a5a10
MWCode <mwcode@163.com>
2021-07-22 22:48:11 +0800

    xxx

---
+++ Test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by MWAdmin on 2021/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by MWAdmin on 2021/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
